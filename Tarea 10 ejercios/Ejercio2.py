c323ec142bcad55df89a1453f314e84c869f14c8
Alfredsanbojorge <ajsanchezb@uamv.edu.ni>
2025-03-29 20:46:19 -0600

    Delete Tarea 10 ejercios directory

--- Tarea 10 ejercios/Ejercio2.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio 2: Calcular la longitud de cada cadena en la lista.
-
-lista_cadenas = ["Hola", "mundo", "esto", "es", "Python"]
-longitudes = [len(cadena) for cadena in lista_cadenas]
-
-print("Longitudes de cada cadena:", longitudes)
--- Tarea 10 ejercios/Ejercio4.py
+++
@@ -1,8 +0,0 @@
-# Ejercicio 4: Buscar si una subcadena está presente en cada cadena de la lista.
-
-lista_cadenas = ["Hola", "mundo", "esto", "es", "Python"]
-subcadena = input("Ingrese la subcadena a buscar: ")
-
-resultados = [subcadena in cadena for cadena in lista_cadenas]
-
-print("Resultados de búsqueda:", resultados)
