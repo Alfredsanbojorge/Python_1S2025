80321287b766364308ed478828e5e73f809e26dc
Alfredsanbojorge <ajsanchezb@uamv.edu.ni>
2025-03-29 20:43:05 -0600

    Delete Tarea 10 ejercios directory

--- Tarea 10 ejercios/Ejercio1.py
+++
@@ -1,7 +0,0 @@
-# Ejercicio 1: Concatenar todas las cadenas en una sola cadena separada por un espacio
-
-def concatenar_cadenas(lista):
-    return ' '.join(lista)
-
-lista_cadenas = ['Hola', 'mundo', 'esto', 'es', 'Python']
-print(concatenar_cadenas(lista_cadenas))
