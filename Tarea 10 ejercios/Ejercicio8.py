d4f024c718a379337876068063802969bc72b8be
Alfredsanbojorge <ajsanchezb@uamv.edu.ni>
2025-03-29 20:45:25 -0600

    Primer comit
    Adjunto los 10 ejercicios de tarea

---
+++ Tarea 10 ejercios/Ejercicio8.py
@@ -0,0 +1,6 @@
+# Ejercicio 8: Ordenar la lista de cadenas alfabéticamente.
+
+lista_cadenas = ["Hola", "mundo", "esto", "es", "Python"]
+ordenadas = sorted(lista_cadenas)
+
+print("Lista ordenada:", ordenadas)
